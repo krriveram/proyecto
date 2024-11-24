288eb3e9ff77cc1b8b1a98afeab601f2efff44d9
Kamila Rivera <krrivera@espol.edu.ec>
2024-11-24 03:44:09 -0500

    Actualizacion del Deck, lectura del archivo y cambios en robarCarta

--- Deck.py
+++ Deck.py
@@ -1,12 +1,31 @@
 import random
-import numpy as np
 from CartaYuGiOh import CartaYuGiOh
-
+import TipoCarta
 class Deck:
-    def __init__(self, cartasDeck):
-        self.cartasDeck = cartasDeck
+    
+    def __init__(self, cartasDeck, archivo):
+        cartasDeck=[]
+        contMon=0
+        contTram=-0
+        contMag=0
+        with open(archivo, 'r') as deckCartas:
+            for linea in deckCartas:
+                posicionAleatoria=random.randint(1,28)
+                carta=linea.strip('\n').split('/')[posicionAleatoria]
+                while contMon<20 or contTram<5 or contMag<5:
+                    if carta[5]==TipoCarta.MONSTRUO.value:
+                        cartasDeck.add(carta)
+                        contMon+=1
+                    elif carta[5]==TipoCarta.TRAMPA.value:
+                       cartasDeck.add(carta)
+                       contTram+=1
+                    elif carta[5]==TipoCarta.MAGICA.value:
+                        cartasDeck.add(carta)
+                        contMag+=1
+        self.cartasDeck = cartasDeck    
 
     def robar_carta(self):
         if self.cartasDeck:
-            return self.cartasDeck.pop(0)
+            cartAle=random.randint(1,49)
+            return self.cartasDeck.pop(cartAle)
         return None
