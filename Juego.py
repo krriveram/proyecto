3765d9cfd92ba5e0e0d5d809e95478ce4ef9b9ac
johanbasi2305 <johabasi@espol.edu.ec>
2024-11-23 23:53:25 -0500

    Update Juego.py

--- Juego.py
+++ Juego.py
@@ -1,2 +1,18 @@
-import random as rd
+from tablero import Tablero
+from jugador import Jugador
+from deck import Deck
+from carta import CartaYuGiOh, Monstruo, Magica, Trampa
+from enum import Enum
+
+class Turno(Enum):
+  JUGADOR = 1
+  MAQUINA = 2
+
+class Juego:
+  def __init__(self, jugador, maquina):
+    self.jugador = jugador
+    self.maquina = maquina
+    self.tablero = Tablero()
+    
+
 
