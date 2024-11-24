b777e6d9c84c07aa97c29ee8c0f342173a58133b
johanbasi2305 <johabasi@espol.edu.ec>
2024-11-23 21:36:18 -0500

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -1,13 +1,13 @@
 class Tablero:
   def __init__(self):
-      self.espacioMonstruo = [None] * 3
-      self.espacioTraMag = [None] * 3
+      self.espacio_monstruo_jugador = [None] * 3
+      self.espacio_magtramp_jugador = [None] * 3
+      self.espacio_monstruo_maquina = [None] * 3
+      self.espacio_magtramp_maquina = [None] * 3
 
-  def colocar_monstruo(self, carta, posicion, modo):
-      if 0 <= posicion < 3:
-          self.espacioMonstruo[posicion] = carta
-          carta.cambiar_modo() if modo == 'defensa' else carta.colocar_en_ataque()
-
-  def colocar_magica_trampa(self, carta, posicion):
-      if 0 <= posicion < 3:
-          self.espacioTraMag[posicion] = carta
+    def colocar_carta(self, carta, espacio):
+        if espacio in range(3):
+            if carta.tipo_carta == 'Monstruo':
+                self.espacio_monstruo_jugador[espacio] = carta
+            else:
+                self.espacio_magtramp_jugador[espacio] = carta
