9d4b5c4357242305e0ddc6d4a1c8a00fe683a6e3
Kamila Rivera <krrivera@espol.edu.ec>
2024-11-24 16:57:27 -0500

    reescritura toString

--- Monstruo.py
+++ Monstruo.py
@@ -1,10 +1,9 @@
 from CartaYuGiOh import CartaYuGiOh
 from CartaYuGiOh import TipoCarta
+import numpy as np
 
-class Monstruo:
-
-  class Monstruo(CartaYuGiOh):
-      def __init__(self, nombre, descripcion, atributo, tipo_monstruo, ataque, defensa):
+class Monstruo(CartaYuGiOh):
+    def __init__(self, nombre, descripcion, atributo, tipo_monstruo, ataque, defensa):
         super().__init__(nombre, descripcion, TipoCarta.MONSTRUO)
         self.atributo = atributo
         self.tipo_monstruo = tipo_monstruo
@@ -12,15 +11,86 @@ class Monstruo:
         self.defensa = defensa
         self.modo = "ataque"
         self.boca_arriba = True
-  def atacar(self, enemigo):
-      if enemigo.modo:
-          return self.ataque - enemigo.ataque
-      else:
-          return self.ataque - enemigo.defensa
+    def atacar(self, enemigo):
+        if enemigo.modo:
+            return self.ataque - enemigo.ataque
+        else:
+            return self.ataque - enemigo.defensa
 
-  def cambiar_modo(self):
-      self.modo = not self.modo
+    def cambiar_modo(self):
+        self.modo = not self.modo
 
-  def get_modo(self):
-      return self.modo
-  
+    def get_modo(self):
+        return self.modo
+    
+    def __str__ (self):
+
+        if(self.modo=="ataque"):
+            if(self.boca_arriba==True and "vertical"=="vertical"):
+                arriba=[" * "]
+                cont=self.nombre.split(' ')
+                for i in range(len(cont)):
+                    arriba.append(" * ")
+                arriba.append(" * ")
+                aRab=np.array(arriba)
+                print(aRab)
+                print(f"\n\n\n nombre:"+self.nombre+"\n ataque:"+self.ataque+"\n defensa:"+self.defensa+"\n atributo:"+self.atributo+"\n monstruo:"+self.tipo_monstruo+"\n\n\n")
+                print(aRab)
+            else:
+                M=np.array([[" *", " * ", " * ", " * ", "*"],
+[" * ", "  ", "  ", "  ", " * "],
+[" * ", "  ", "  ", "  ", " * "],
+[" * ", "  ", "  ", "  ", " * "],
+[" * ", "  ", "  ", "  ", " * "],
+[" * ", "  ", "  ", "  ", " * "],
+[" * ", "Yu", "Gi", "Oh"," ! "],
+[" * ", "  ", "  ", "  ", " * "],
+[" * ", "  ", "  ", "  ", " * "],
+[" * ", "  ", "  ", "  ", " * "],
+[" * ", "  ", "  ", "  ", " * "],
+[" * ", "  ", "  ", "  ", " * "],
+["*", " * ", " * ", " * ", "*"]]
+)
+                print(M)
+
+
+            #MODELADO
+            #vertical
+            #boca arriba
+
+            #* * * * * * * *  * * * * * * * * * * *
+            #* nombre: Dragón Negro de Ojos Rojos *
+            #* ataque: 2000                       *
+            #* defensa: 2400                      *
+            #* tipo Atributo: OSCURIDAD           *
+            #* tipo Monstruo: DRAGON              *
+            #* * * * * * * *  * * * * * * * * * * *
+
+            #boca abajo
+            #* * * * * * * *  * * * * *
+            #*                        *
+            #*                        *
+            #*                        *
+            #*       Yu-Gi-Oh         *
+            #*                        *
+            #*                        *
+            #*                        *
+            #* * * * * * * *  * * * * *
+
+
+            #horizontal
+
+            #boca arriba
+
+            #* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+            #*  nombre: Dragón Negro de Ojos Rojos , ataque: 2000 ,defensa: 2400, tipo Atributo: OSCURIDAD , tipo Monstruo: DRAGON *
+            #* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+
+            #boca abajo
+            #* * * * * * * * * * * * * * * * 
+            #*           Yu-Gi-Oh          *
+            #* * * * * * * * * * * * * * * * 
+
+
+
+
