6f961642c3b4892afa0394ef50a438aa7b7fbb93
johanbasi2305 <johabasi@espol.edu.ec>
2024-11-26 20:26:54 -0500

    Update Jugador.py

--- Jugador.py
+++ Jugador.py
@@ -1,36 +1,19 @@
-from Deck import Deck
-from Tablero import Tablero
-import random
-
-
 class Jugador:
-
-    def __init__(self, nombre, deck):
+    def __init__(self, nombre, vida, deck):
         self.nombre = nombre
-        self.vida = 4000
+        self.vida = vida
         self.deck = deck
         self.cartasMano = []
-        self.tablero = Tablero()
 
-    def crearMano(self, cartas):
-        ManoCartas=[]
-        for k in range(5):
-            posicionAleatoria=random.randint(1,len(cartas)-1)
-            mano=cartas[posicionAleatoria]
-            ManoCartas.append(mano)
-        return ManoCartas
-    
-    def robarCartaDeck(self,deck,ManoCartas):
-        self.deck=deck
-        self.ManoCartas=ManoCartas
-        cartaRobada=deck.robarCarta()
-        ManoCartas.append(cartaRobada)
+    def robar_carta(self):
+        if self.deck.cartas:
+            return self.deck.robarCarta()
+        else:
+            return None
 
-
-    def mons_en_tablero(self, carta, posicion, modo):
-        self.tablero.colocar_monstruo(carta, posicion, modo)
-        self.cartasMano.remove(carta)
-
-    def magtram_en_tablero(self, carta, posicion, modo):
-        self.tablero.colocar_magica_trampa(carta, posicion)
-        self.cartasMano.remove(carta)
+    def jugar_carta(self, indice):
+        if 0 <= indice < len(self.cartasMano):
+            return self.cartasMano.pop(indice)
+        else:
+            print("Error: índice fuera de rango.")
+            return None
