05dbef1cf03662dc243305c2c49c9bfeef993b49
Kamila Rivera <krrivera@espol.edu.ec>
2024-12-02 06:37:11 -0500

    Deck corregido

--- Deck.py
+++ Deck.py
@@ -20,50 +20,45 @@ class Deck:
 
         cartas = []
 
-        try:
-            with open(ruta_absoluta, 'r', encoding='utf-8') as file:
-                for linea in file:
-                    partes = linea.strip().split('/')
-                    
-                    if partes[0].lower() == "monstruo":
-                        if len(partes) < 7:
-                            print(f"Error: Falta información en la línea: {linea}")
-                            continue
-                        nombre = partes[1]
-                        descripcion = partes[2]
-                        ataque = partes[3]
-                        defensa = partes[4]
-                        atributo = partes[5]
-                        tipo_monstruo = partes[6]
-                        carta = Monstruo(nombre, descripcion, ataque, defensa, atributo, tipo_monstruo)
-                    elif partes[0].lower() == "magica":
-                        if len(partes) < 5:
-                            print(f"Error: Falta información en la línea: {linea}")
-                            continue
-                        nombre = partes[1]
-                        descripcion = partes[2]
-                        tipo_objetivo = partes[3]
-                        incremento = partes[4]
-                        carta = Magica(nombre, descripcion, tipo_objetivo, incremento)
-                    elif partes[0].lower() == "trampa":
-                        if len(partes) < 4:
-                            print(f"Error: Falta información en la línea: {linea}")
-                            continue
-                        nombre = partes[1]
-                        descripcion = partes[2]
-                        tipo_objetivo = partes[3]
-                        carta = Trampa(nombre, descripcion, tipo_objetivo)
-                    else:
-                        print(f"Tipo de carta desconocido: {partes[0]}")
+        with open(ruta_absoluta, 'r', encoding='utf-8') as file:
+            for linea in file:
+                partes = linea.strip().split('/')
+                
+                if partes[0].lower() == "monstruo":
+                    if len(partes) < 7:
+                        print(f"Error: Falta información en la línea: {linea}")
                         continue
+                    nombre = partes[1]
+                    descripcion = partes[2]
+                    ataque = partes[3]
+                    defensa = partes[4]
+                    atributo = partes[5]
+                    tipo_monstruo = partes[6]
+                    carta = Monstruo(nombre, descripcion, ataque, defensa, atributo, tipo_monstruo)
+                elif partes[0].lower() == "magica":
+                    if len(partes) < 5:
+                        print(f"Error: Falta información en la línea: {linea}")
+                        continue
+                    nombre = partes[1]
+                    descripcion = partes[2]
+                    tipo_objetivo = partes[3]
+                    incremento = partes[4]
+                    carta = Magica(nombre, descripcion, tipo_objetivo, incremento)
+                elif partes[0].lower() == "trampa":
+                    if len(partes) < 4:
+                        print(f"Error: Falta información en la línea: {linea}")
+                        continue
+                    nombre = partes[1]
+                    descripcion = partes[2]
+                    tipo_objetivo = partes[3]
+                    carta = Trampa(nombre, descripcion, tipo_objetivo)
+                else:
+                    print(f"Tipo de carta desconocido: {partes[0]}")
+                    continue
 
-                    cartas.append(carta)
-        except FileNotFoundError:
-            print(f"Error: El archivo '{archivo}' no se encontró.")
-        except Exception as e:
-            print(f"Ocurrió un error: {e}")
+                cartas.append(carta)
 
-        return cartas
+            return cartas
 
     @staticmethod
     def generar_deck():
